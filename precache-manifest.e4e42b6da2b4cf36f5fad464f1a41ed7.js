51dc269c7cd4fd331aec7affcf8cbee78062e5da
Arif Luthfi <arifluthfi16@gmail.com>
2019-08-27 13:16:04 +0700

    Updates

---
+++ precache-manifest.e4e42b6da2b4cf36f5fad464f1a41ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be690d33127a1bd8e55dbc49455a855",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "d5a62030d989da8b6f0f",
+    "url": "/monster-rolodex/static/css/main.54f08459.chunk.css"
+  },
+  {
+    "revision": "e6f182edb81f551cb0c1",
+    "url": "/monster-rolodex/static/js/2.a4cf3094.chunk.js"
+  },
+  {
+    "revision": "d5a62030d989da8b6f0f",
+    "url": "/monster-rolodex/static/js/main.11d1f0c1.chunk.js"
+  },
+  {
+    "revision": "1f1fdb189afd82863836",
+    "url": "/monster-rolodex/static/js/runtime~main.80659f76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
